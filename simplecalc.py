2f630041fdda6f80dad5d9b1fc5b522ceb5beb42
Swayan Basu <207083552+swayanbasu04@users.noreply.github.com>
2026-07-03 06:21:04 +0000

    Encapsulate calculator logic in main function
    Refactored calculator code into a main function with improved error handling and user prompts.

--- simplecalc.py
+++ simplecalc.py
@@ -1,47 +1,54 @@
-while True:
-    print("1-Add")
-    print("2-substract")
-    print("3-multipication")
-    print("4-division")
-    print("5-remainder")
-    try:
-        option=int(input("choose an operation(between 1to5): "))
-    except ValueError:
-        print("invalid input. please enter a number between 1 and 5.")
-        continue
-    
-    if option == 5:
-        print("Exiting calculator. Goodbye!")
-        break
+def main():
+    """Run a simple calculator until the user exits."""
+    while True:
+        print("\n1 - Add")
+        print("2 - Subtract")
+        print("3 - Multiply")
+        print("4 - Divide")
+        print("5 - Remainder")
+        print("6 - Exit")
 
-    if option not in [1,2,3,4,5]:
-        print("Invalid option! Please choose between 1-5.")
-        continue
+        try:
+            option = int(input("Choose an operation (1-6): "))
+        except ValueError:
+            print("Invalid input. Enter a number between 1 and 6.")
+            continue
 
-    try:
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-    except ValueError:
-        print("Invalid number entered!")
-        continue
+        if option == 6:
+            print("Exiting calculator. Goodbye!")
+            break
 
-    print("you have selected option ", option)
+        if option not in [1, 2, 3, 4, 5]:
+            print("Invalid option! Choose between 1 and 6.")
+            continue
 
-    if (option==1):
-        result=num1+num2
-    elif (option==2):
-        result=num1-num2
-    elif (option==3):
-        result=num1*num2
-    elif (option==4):
-        result=num1/num2
-    elif (option==5):
-        result=num1%num2
-    print("the calculated result is: ", result)
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid number entered!")
+            continue
+
+        try:
+            if option == 1:
+                result = num1 + num2
+            elif option == 2:
+                result = num1 - num2
+            elif option == 3:
+                result = num1 * num2
+            elif option == 4:
+                result = num1 / num2
+            elif option == 5:
+                result = num1 % num2
+        except ZeroDivisionError:
+            print("Error: Division by zero is not allowed.")
+            continue
+
+        print("The calculated result is:", result)
+
 
 if __name__ == "__main__":
     main()
-
 
 
 
